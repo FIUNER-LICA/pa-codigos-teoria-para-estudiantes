36a99d0b64b3f336b8ea4d93dac0af081547a11e
dz <100449246+dzjavier@users.noreply.github.com>
2022-10-23 09:28:46 -0300

    Se agrega ejemplo de app simulacion

---
+++ orquesta/tests/test_orchestra.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  4 19:17:42 2022
+
+@author: DELL
+"""
+
+import unittest 
+
+class TestOrchestra(unittest.TestCase):
+    pass
+
+    def setUp(self):
+        pass        
+    
+    def tearDown(self):
+        pass
+    
+if __name__ == "__main__":
+    unittest.main()
---
+++ orquesta/tests/test_violin.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  4 19:15:10 2022
+
+@author: DELL
+"""
+
+import unittest 
+
+class TestViolin(unittest.TestCase):
+    pass
+
+    def setUp(self):
+        pass        
+    
+    def tearDown(self):
+        pass
+    
+if __name__ == "__main__":
+    unittest.main()
