1287c053a24413aacd000dec181447d3ebc552ac
dz <100449246+dzjavier@users.noreply.github.com>
2023-03-18 15:41:52 -0300

    Se eliminan archivos cursada anterior
    Se eliminan archivos cursada anterior, quedan en una rama específica del cursado

--- graficacion-parte-I/figura_simple_controlando_tamanio_y_rel_aspecto_mas_compacto.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-from matplotlib import pyplot as plt
-
-
-fig, ax = plt.subplots(figsize=(6,6), subplot_kw={"aspect":1})
-ax.plot(range(10))
-plt.show()
--- orquesta/modules/orchestra.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Sep  4 19:17:28 2022
-
-@author: DELL
-"""
-
