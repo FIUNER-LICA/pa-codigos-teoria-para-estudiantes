53f8851905f0438802f2b0f255a8b1d4d50064a2
jinsfran <jordan.insfran@uner.edu.ar>
2024-06-10 19:15:47 -0300

    Se separa parámetro para ubicar BD de función que crea el motor (engine) para interacciónar con ella.

--- POO/desacople/modulos/configuraciones.py
+++ POO/desacople/modulos/configuraciones.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine
 
+URL_BD = "sqlite:///POO/desacople/datos/base_de_datos.db"
+
 def crear_engine():
-    engine = create_engine("sqlite:///POO/desacople/datos/base_de_datos.db")
+    engine = create_engine(URL_BD)
     return engine
